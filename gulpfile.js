a3a87c4500481ea21a941e6cb2f744fd27d865a8
superluckyfir <superluckyfir@qq.com>
2022-05-26 12:02:08 +0800

    Refactor
    1. Update dependencies
    2. Use builtin modules instead of global function
    3. Fix nested list recursion bug in splice under functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,46 @@
-const gulp = require('gulp');
-const sassdoc = require('sassdoc');
+const { src, dest, parallel, watch } = require('gulp');
+const _sassdoc = require('sassdoc');
 const mocha = require('gulp-mocha');
 
-gulp.task('default', () =>
-	gulp.src('./test/test_sass.js')
-		.pipe(mocha())
-		.once('error', () => {
-			process.exit(1);
-		})
-		.once('end', () => {
-			process.exit();
-		})
-);
+function test(done) {
+  src('./test/test_sass.js')
+    .pipe(mocha())
+    .once('error', () => {
+      process.exit(1);
+    })
+    .once('end', () => {
+      process.exit();
+    });
+  done();
+}
 
-gulp.task('sassdoc', () => {
-	return gulp.src(['_config.scss', '_bem.scss', './mixins/**.scss', './helpers/**.scss', './functions/*.scss'])
-		.pipe(sassdoc({
-			dest: 'gh-pages',
-			verbose: true,
-			display: {
-				access: ['public', 'private'],
-				alias: true,
-				watermark: true,
-			},
-			groups: {
-				'undefined': 'sass-bem'
-			},
-			basePath: 'http://zgabievi.me/sass-bem/',
-		}))
-});
+function sassdoc(done) {
+  src([
+    '_config.scss',
+    '_bem.scss',
+    './mixins/**.scss',
+    './helpers/**.scss',
+    './functions/*.scss'
+  ]).pipe(
+    _sassdoc({
+      dest: 'gh-pages',
+      verbose: true,
+      display: {
+        access: ['public', 'private'],
+        alias: true,
+        watermark: true
+      },
+      groups: {
+        undefined: 'sass-bem'
+      },
+      basePath: 'http://zgabievi.me/sass-bem/'
+    })
+  );
+	done();
+}
+
+exports.sassdoc = sassdoc;
+exports.test = test;
+exports.default = function () {
+  watch('./src/**/*.scss', test);
+}
--- test/test_sass.js
+++ test/test_sass.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const sass_true = require('sass-true');
+const path = require('path')
+const sassTrue = require('sass-true')
 
-const sassFile = path.join(__dirname, 'test.scss');
-sass_true.runSass({file: sassFile}, describe, it);
+const sassFile = path.join(__dirname, 'test.scss')
+sassTrue.runSass({ file: sassFile }, { describe, it })
